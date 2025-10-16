9d78b1a58f0e62d0c24bcedd68c9cff38961cf32
Alan D. <alan@nexlevsolutions.com>
2025-10-16 17:59:05 -0600

    Avance de mi componente

---
+++ src/app/adiaz/adiaz.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adiaz',
+  imports: [],
+  templateUrl: './adiaz.component.html',
+  styleUrl: './adiaz.component.css'
+})
+export class AdiazComponent {
+  name = 'Alan Díaz';
+  description = 'Desarrollador Web | Angular Lover | Otaku Fullstack';
+  imageUrl = 'https://i.pinimg.com/originals/5c/33/3d/5c333d37c1d0a46a52ce1fc6ea4b1fa4.jpg'; // puedes cambiarla
+  email = 'alan.diaz@example.com';
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdiazComponent } from './adiaz/adiaz.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AdiazComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
